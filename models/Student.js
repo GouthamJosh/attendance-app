3c6d9b1ba0aff15496866fe13da9521efe010a6d
GouthamJosh <96323721+GouthamJosh@users.noreply.github.com>
2026-01-09 21:08:58 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const StudentSchema = new mongoose.Schema({
   name: String,
   rollNo: String,
-  className: String
+  className: String,
+  division: String,
+  semester: String
 });
 
-module.exports = mongoose.model("Student", StudentSchema);
+module.exports =
+  mongoose.models.Student ||
+  mongoose.model("Student", StudentSchema);
