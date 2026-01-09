a1ed3b6507f1b3870c924476f988aee61d90938f
GouthamJosh <96323721+GouthamJosh@users.noreply.github.com>
2026-01-09 21:10:04 +0530

    Update attendanceRoutes.js

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -3,19 +3,12 @@ const Attendance = require("../models/Attendance");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-/* PREVENT DUPLICATE ATTENDANCE */
 router.post("/", auth, async (req, res) => {
   const exists = await Attendance.findOne({ date: req.body.date });
   if (exists) return res.status(400).json("Attendance already marked");
 
   await new Attendance(req.body).save();
-  res.json("Attendance Saved");
-});
-
-router.get("/:date", auth, async (req, res) => {
-  const data = await Attendance.findOne({ date: req.params.date })
-    .populate("records.studentId");
-  res.json(data);
+  res.json("Attendance saved successfully");
 });
 
 module.exports = router;
