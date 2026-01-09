e29904c01a2d10a03ffdf82516b0833281f60203
GouthamJosh <96323721+GouthamJosh@users.noreply.github.com>
2026-01-09 20:30:43 +0530

    Delete attendance-app directory

--- attendance-app/models/Teacher.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TeacherSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
-
-module.exports = mongoose.model("Teacher", TeacherSchema);
